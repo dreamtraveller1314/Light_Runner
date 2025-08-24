ce239848bfcbb8463f8bbe7a4010e1b934f6e977
__RU__ <xinru2509@gmail.com>
2025-08-24 21:43:59 +0800

    adding level

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -200.0
+const JUMP_VELOCITY = -600.0
 
 
 func _physics_process(delta: float) -> void:
@@ -10,7 +10,7 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
@@ -20,5 +20,14 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	if Input.is_action_pressed("ui_left"):
+		$AnimatedSprite2D.flip_h = false
+		$AnimatedSprite2D.play("default")
+	elif Input.is_action_pressed("ui_right"):
+		$AnimatedSprite2D.flip_h = true
+		$AnimatedSprite2D.play("default")
+	else:
+		$AnimatedSprite2D.animation = ("new_animation")
 
 	move_and_slide()
